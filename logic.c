94020d6d2446c139cb338cbc590e3c8ae3ea28f6
mjklmg <michit7312@proton.me>
2026-05-20 17:33:53 +0200

    added logic, game loop and movement

---
+++ logic.c
@@ -0,0 +1,57 @@
+#include "snake.h"
+#include <stdbool.h>
+
+_Bool isDirectionLegal(SnakeBody *body, DIRECTION inputDir){
+
+	// checks if the direction is legal, e. g. u cannot move LEFT while moving RIGHT
+
+	DIRECTION currentDir = body->dir;
+	switch (inputDir) {
+		case UP:
+			if (currentDir==DOWN){
+				return false;
+			}
+			break;
+		case DOWN:
+			if (currentDir==UP){
+				return false;
+			}
+			break;
+		case LEFT:
+			if (currentDir==RIGHT){
+				return false;
+			}
+			break;
+		case RIGHT:
+			if (currentDir==LEFT){
+				return false;
+			}
+			break;
+	}
+	
+	return true;
+}
+
+void moveSnake(SnakeBody *body, DIRECTION inputDir){
+
+	body->dir = inputDir;
+
+	addHead(body);
+	deleteTail(body);
+}
+
+_Bool touchedBorder(SnakeBody *body, int boardHeight, int boardWidth ){
+
+	// importing head coordinates
+	int X = body->head->posX;
+	int Y = body->head->posY;
+
+	// checking if head touched the border
+	if (X<=0 || X>=boardWidth-1){
+		return true;
+	}
+	else if (Y<=0 || Y>=boardHeight-1){
+		return true;
+	}
+	return false;
+}
---
+++ logic.h
@@ -0,0 +1,17 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+
+#include "snake.h"
+#include <stdbool.h>
+
+// checks if the direction is legal, e. g. u cannot move LEFT while moving RIGHT
+_Bool isDirectionLegal(SnakeBody *body, DIRECTION inputDir);
+
+// handles snakes movement
+void moveSnake(SnakeBody *body, DIRECTION inputDir);
+
+// checks if border was touched
+_Bool touchedBorder(SnakeBody *body, int boardHeight, int boardWidth);
+
+#endif
